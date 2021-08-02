b594bc2defa73b5a96fa70662edb85e58ca38982
jonasdevzero <jonasdevzero@gmail.com>
2021-08-02 14:56:51 -0300

    finish user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,9 @@
-import { getRepository, ILike } from "typeorm"
+import { getRepository, ILike, Not } from "typeorm"
 import { ServerRequest, ServerReply } from "../types/controller"
 import User from "../models/User"
-import { mailer, userUtil } from "../utils"
+import { userUtil, upload } from "../utils"
 import * as yup from "yup"
+import * as mailer from "../mailer"
 
 export default {
     async index(_req: ServerRequest, reply: ServerReply) {
@@ -48,16 +49,16 @@ export default {
 
     async create(req: ServerRequest, reply: ServerReply) {
         try {
-            const { name, username, email, password, confirmPassword } = req.body
+            const { name, username, email, password, confirm_password } = req.body
 
-            if (password !== confirmPassword)
+            if (password !== confirm_password)
                 return reply.status(500).send({ message: "Senhas diferentes" })
 
             const schema = yup.object().shape({
-                confirmPassword: yup.string().min(6).required("Preencha o campo 'confirmar senha'"),
+                confirm_password: yup.string().min(6).required("Preencha o campo 'confirmar senha'"),
                 password: yup.string().min(6, "A senha deve ter no mínimo 6 caracteres").required("Preencha o campo 'senha'"),
                 email: yup.string().lowercase().email("Formatação de email incorreta").required("Preencha o campo 'email'"),
-                username: yup.string().lowercase().min(4, "O nome de usuário deve ter no mínimo 4 caracteres").required("Preencha o campo 'username'"),
+                username: yup.string().lowercase().min(4, "O username deve ter no mínimo 4 caracteres").required("Preencha o campo 'username'"),
                 name: yup.string().required("Preencha o campo 'nome'"),
             })
 
@@ -125,7 +126,7 @@ export default {
             if (user?.deleted_at)
                 return reply.status(400).send({ message: "Sua conta foi deletada! Restaure-a!" })
 
-            reply.status(200).send({ user, auth: true })
+            reply.status(200).send({ user })
         } catch (error) {
             reply.status(500).send({ message: "Internal Server Error", error })
         }
@@ -133,7 +134,105 @@ export default {
 
     async update(req: ServerRequest, reply: ServerReply) {
         try {
+            const id = req.user.toString()
+            const { name, username } = req.body
 
+            const userRepository = getRepository(User)
+
+            const scheme = yup.object({
+                name: yup.string().required("Preencha o campo 'nome'"),
+                username: yup.string().min(4, "O username deve ter no mínimo 4 caracteres").required("Preencha o campo 'username'")
+            })
+            let validationError: any = undefined
+
+            const [user, existsUsername] = await Promise.all([
+                userRepository.findOne(id),
+                userRepository.findOne({ where: { username, id: Not(id) } }),
+                scheme.validate(req.body).catch(error => validationError = error)
+            ])
+
+            if (validationError)
+                return reply.status(400).send({ message: validationError.errors[0] })
+
+            if (!user)
+                return reply.status(500).send({ message: "Usuário não encontrado!" })
+
+            if (existsUsername)
+                return reply.status(400).send({ message: "Username em uso!" })
+
+            await userRepository.update(id, { name, username })
+            return reply.status(200).send({ name, username })
+        } catch (error) {
+            reply.status(500).send({ message: "Internal Server Error", error })
+        }
+    },
+
+    async email(req: ServerRequest, reply: ServerReply) {
+        try {
+            const id = req.user.toString()
+            const { email, password } = req.body
+            
+            const schema = yup.object({
+                email: yup.string().email("Email mal formatado").required("Preencha o campo 'email'"),
+                password: yup.string().required("Preencha o campo  'senha'")
+            })
+            let validationError: any = undefined
+
+            const userRepository = getRepository(User)
+            const [user, existsEmail] = await Promise.all([
+                userRepository.findOne(id),
+                userRepository.findOne({ where: { id: Not(id), email } }),
+                schema.validate(req.body).catch(error => validationError = error)
+            ])
+
+            if (validationError)
+                return reply.status(400).send({ message: validationError.errors[0] })
+
+            if (!user)
+                return reply.status(500).send({ message: "Usuário não encontrado!" })
+
+            if (existsEmail)
+                return reply.status(400).send({ message: "Email em uso!" })
+
+            if (!userUtil.comparePasswords(password, user.password))
+                return reply.status(401).send({ message: "Senha incorreta!" })
+
+            await userRepository.update(id, { email })
+
+            reply.status(200).send({ email })
+        } catch (error) {
+            reply.status(500).send({ message: "Internal Server Error", error })
+        }
+    },
+
+    async picture(req: ServerRequest, reply: ServerReply) {
+        try {
+            const id = req.user.toString()
+            const { picture } = upload.parseBody(req.body)
+
+            const userRepository = getRepository(User)
+            const user = await userRepository.findOne(id)
+
+            if (!user)
+                return reply.status(500).send({ message: "Error inesperado! Tente novamente" })
+
+            let location = null
+            if (picture) {
+                const [{ Location }] = await Promise.all([
+                    upload.save(picture),
+                    user.picture ? upload.remove(user.picture) : null
+                ]).catch((error) => reply.status(500).send({ message: "Não foi possível atualizar a imagem!", error }))
+
+                location = Location
+                await userRepository.update(id, { picture: Location })
+            } else if (user.picture) {
+                await Promise.all([
+                    upload.remove(user.picture),
+                    userRepository.update(id, { picture: undefined })
+                ]).catch(() => reply.status(500).send({ message: 'Não foi possível remover a imagem!' }))
+            }
+
+            reply.status(200).send({ location })
         } catch (error) {
             reply.status(500).send({ message: "Internal Server Error", error })
         }
@@ -153,17 +252,15 @@ export default {
                 return reply.status(400).send({ message: "Sua conta foi deletada! Restaure-a!" })
 
             const reset_token = userUtil.generateHash()
-            const expire_token = new Date().setHours(new Date().getHours() + 1)
+            const expire_token = new Date()
+            expire_token.setHours(new Date().getHours() + 1)
 
             const { id } = user
             await userRepository.update(id, { reset_token, expire_token })
 
-            mailer.sendMail({
-                from: "Dev Zero <devzerotest@gmail.com>",
-                to: email,
-                subject: "Password Reset",
-                html: `<h1>Click <a href="${process.env.CLIENT_URL}/resetPassword/${reset_token}">here</a> to reset your password</h1>`,
-            })
+            const link = `${process.env.CLIENT_URL}/resetPassword/${reset_token}`
+            const template = await mailer.loadTemplate("forgotPassword", { link })
+            mailer.sendMail(email, "Password Reset", template)
 
             reply.status(200).send({ message: 'Verifique seu email' })
         } catch (error) {
@@ -173,10 +270,21 @@ export default {
 
     async resetPassword(req: ServerRequest, reply: ServerReply) {
         try {
-            const { password, confirmPassword, reset_token } = req.body
+            const { password, confirm_password, reset_token } = req.body
 
-            if (password !== confirmPassword)
+            if (password !== confirm_password)
                 return reply.status(400).send({ message: 'Senhas diferentes' })
+
+            const schema = yup.object({
+                reset_token: yup.string().required("Token para resetar está faltando"),
+                password: yup.string().min(6, "A senha deve ter no mínimo 6 caracteres").required("Preencha o campo senha")
+            })
+
+            let validationError: any = undefined
+            schema.validate(req.body).catch(err => validationError = err)
+
+            if (validationError)
+                return reply.status(400).send({ message: validationError.errors[0] })
 
             const userRepository = getRepository(User)
             const user = await userRepository.findOne({ where: { reset_token } })
