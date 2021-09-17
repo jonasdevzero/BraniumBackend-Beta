afcebe6dd628255f0893fedf94823f4044df51e2
jonasdevzero <jonasdevzero@gmail.com>
2021-09-17 17:33:47 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,20 @@ import path from "path"
 import routes from "./routes"
 import { socketServer } from "./socket"
 
+const port = process.env.PORT || 5000
+const host = process.env.HOST || "127.0.0.1"
 const secret = process.env.USER_SECRET || "zero"
 const errorJwtMessages = {
     badRequestErrorMessage: "Sessão inválida!",
     noAuthorizationInHeaderMessage: "Sessão não encontrada!",
     authorizationTokenExpiredMessage: "Sessão expirada!",
 }
+
+console.log("[port]", port)
+console.log("[process port]", process.env.PORT)
+
+console.log("[host]", host)
+console.log("[process host]", process.env.HOST)
 
 if (cluster.isPrimary) {
     for (let i = 0; i < cpus().length; i++) {
@@ -36,7 +44,7 @@ if (cluster.isPrimary) {
     server.register(fastifyMultipart, { attachFieldsToBody: true })
     server.register(routes)
 
-    server.listen(process.env.PORT || 5000, process.env.HOST || "127.0.0.1", async (err, address) => {
+    server.listen(port, host, async (err, address) => {
         if (err) {
             server.log.error(err)
             process.exit(1)
