68d5cd272e5095a210ef4fd7443c4f2396c2fe9b
jonasdevzero <jonasdevzero@gmail.com>
2021-09-17 00:11:55 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import fastifyCors from "fastify-cors"
 import fastifyJwt from "fastify-jwt"
 import fastifyMultipart from "fastify-multipart"
 import fastifyStatic from "fastify-static"
-import cluster from "cluster"
-import { cpus } from "os"
 import path from "path"
 import routes from "./routes"
 import { socketServer } from "./socket"
@@ -20,31 +18,20 @@ const errorJwtMessages = {
     authorizationTokenExpiredMessage: "Sessão expirada!",
 }
 
-if (cluster.isPrimary) {
-    for (let i = 0; i < cpus().length; i++) {
-        cluster.fork()
+const server = fastify({ logger: true })
+
+server.register(fastifyCors, { origin: "*", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] })
+server.register(fastifyJwt, { secret, messages: errorJwtMessages })
+server.register(fastifyStatic, { root: path.join(__dirname, "..", "uploads") })
+server.register(fastifyMultipart, { attachFieldsToBody: true })
+server.register(routes)
+
+server.listen(port, host, async (err, address) => {
+    if (err) {
+        server.log.error(err)
+        process.exit(1)
     }
 
-    cluster.on("exit", worker => {
-        console.log(`Process ${worker.process.pid} died`)
-        cluster.fork()
-    })
-} else {
-    const server = fastify({ logger: true })
-    
-    server.register(fastifyCors, { origin: "*", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] })
-    server.register(fastifyJwt, { secret, messages: errorJwtMessages })
-    server.register(fastifyStatic, { root: path.join(__dirname, "..", "uploads") })
-    server.register(fastifyMultipart, { attachFieldsToBody: true })
-    server.register(routes)
-
-    server.listen(port, host, async (err, address) => {
-        if (err) {
-            server.log.error(err)
-            process.exit(1)
-        }
-
-        socketServer(server)
-        server.log.info(`server listening on ${address}`)
-    })
-}
+    socketServer(server)
+    server.log.info(`server listening on ${address}`)
+})
