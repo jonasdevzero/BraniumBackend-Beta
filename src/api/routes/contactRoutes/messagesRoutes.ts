6c28a212d7711c21514ff99f319ce9147db15985
jonasdevzero <jonasdevzero@gmail.com>
2022-01-11 21:43:09 -0300

    adding contact messages routes tests

--- src/api/routes/contactRoutes/messagesRoutes.ts
+++ src/api/routes/contactRoutes/messagesRoutes.ts
@@ -1,27 +1,51 @@
-import { FastifyPluginOptions, FastifyInstance } from "fastify"
-import MessagesController from "../../controllers/ContactController/MessagesController"
-import schema from "../_schema/contactSchema/messagesSchema"
+import { FastifyPluginOptions, FastifyInstance } from 'fastify';
+import MessagesController from '../../controllers/ContactController/MessagesController';
+import schema from '../_schema/contactSchema/messagesSchema';
 
-export default function contactMessagesRoutes(fastify: FastifyInstance, _opts: FastifyPluginOptions, done: (err?: Error) => void) {
-    fastify.get("/:contact", {
-        schema: schema.index,
-    }, MessagesController.index)
+export default function contactMessagesRoutes(
+    fastify: FastifyInstance,
+    _opts: FastifyPluginOptions,
+    done: (err?: Error) => void,
+) {
+    fastify.get(
+        '/:contact',
+        {
+            schema: schema.index,
+        },
+        MessagesController.index,
+    );
 
-    fastify.post("/", {
-        schema: schema.create,
-    }, MessagesController.create)
+    fastify.post(
+        '/',
+        {
+            schema: schema.create,
+        },
+        MessagesController.create,
+    );
 
-    fastify.patch("/view/:contact", {
-        schema: schema.view,
-    }, MessagesController.view)
+    fastify.patch(
+        '/view/:contact',
+        {
+            schema: schema.view,
+        },
+        MessagesController.view,
+    );
 
-    fastify.delete("/:message", {
-        schema: schema.deleteOne,
-    }, MessagesController.deleteOne)
+    fastify.delete(
+        '/:message',
+        {
+            schema: schema.deleteOne,
+        },
+        MessagesController.deleteOne,
+    );
 
-    fastify.delete("/clear/:contact", {
-        schema: schema.clear,
-    }, MessagesController.clear)
+    fastify.delete(
+        '/clear/:contact',
+        {
+            schema: schema.clear,
+        },
+        MessagesController.clear,
+    );
 
-    done()
+    done();
 }
--- tests/routes/contact.test.ts
+++ tests/routes/contact.test.ts
@@ -165,6 +165,7 @@ describe('contact routes tests', () => {
     });
 
     test('PATCH /contact/block', async () => {
+        // user_1 blocking user_0
         const res = await app.inject({
             method: 'PATCH',
             url: `/contact/block/${user_0.id}`,
@@ -180,11 +181,235 @@ describe('contact routes tests', () => {
                 authorization: users[0].jwt,
             },
         });
+        const res3 = await app.inject({
+            method: 'POST',
+            url: '/user/auth',
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
 
-        const { user } = JSON.parse(res2.payload);
-        user_0 = user;
+        user_0 = JSON.parse(res2.payload).user;
+        user_1 = JSON.parse(res3.payload).user;
 
         expect(res.statusCode).toEqual(200);
         expect(user_0.contacts[0].blocked).toBeTruthy();
+        expect(user_1.contacts[0].you_blocked).toBeTruthy();
+    });
+
+    // ------------- testing contact messages routes -------------
+
+    test('POST /contact/messages', async () => {
+        // unblock contact from the last test
+        await app.inject({
+            method: 'PATCH',
+            url: `/contact/block/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const form = new FormData();
+        form.append('to', user_1.id);
+        form.append('text', 'testing create message');
+
+        // user_0 sending a message to user_1
+        const res = await app.inject({
+            method: 'POST',
+            url: '/contact/messages',
+            payload: form,
+            headers: {
+                ...form.getHeaders(),
+                authorization: users[0].jwt,
+            },
+        });
+
+        expect(res.statusCode).toEqual(201);
+    });
+
+    test('GET /contact/messages/:contact', async () => {
+        const res = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const { messages } = JSON.parse(res.payload);
+        const { sender_id, text } = messages[0];
+
+        expect({ sender_id, text }).toEqual({
+            sender_id: user_0.id,
+            text: 'testing create message',
+        });
+    });
+
+    test('PATCH /contact/messages/view/:contact', async () => {
+        const res = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const beforeMessage = JSON.parse(res.payload).messages[0];
+
+        // user_1 viewing the message from user_0
+        await app.inject({
+            method: 'PATCH',
+            url: `/contact/messages/view/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const res2 = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const afterMessage = JSON.parse(res2.payload).messages[0];
+
+        expect(beforeMessage.viewed).toStrictEqual(false);
+        expect(afterMessage.viewed).toStrictEqual(true);
+    });
+
+    test('DELETE /contact/messages/:message', async () => {
+        const res = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_1.id}`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+
+        const message_id = JSON.parse(res.payload).messages[0].id;
+
+        await app.inject({
+            method: 'DELETE',
+            url: `/contact/messages/${message_id}`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+
+        const res2 = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_1.id}`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+        const res3 = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const user_0_messages = JSON.parse(res2.payload).messages;
+        const user_1_messages = JSON.parse(res3.payload).messages;
+
+        expect(user_0_messages.length).toEqual(0);
+        expect(user_1_messages.length).toEqual(1);
+    });
+
+    test('DELETE /contact/messages/clear/:contact', async () => {
+        const form = new FormData();
+        form.append('to', user_1.id);
+        form.append('text', `create message 2`);
+        // user_0 sending a message to user_1
+        await app.inject({
+            method: 'POST',
+            url: '/contact/messages',
+            payload: form,
+            headers: {
+                ...form.getHeaders(),
+                authorization: users[0].jwt,
+            },
+        });
+
+        // clearing messages that user_0 sent
+        await app.inject({
+            method: 'DELETE',
+            url: `/contact/messages/clear/${user_1.id}`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+
+        const res = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_1.id}`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+        const res2 = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const user_0_messages = JSON.parse(res.payload).messages;
+        const user_1_messages = JSON.parse(res2.payload).messages;
+
+        expect(user_0_messages.length).toEqual(0);
+        expect(user_1_messages.length).toEqual(2);
+    });
+
+    test('DELETE /contact/messages/:message?target=bidirectional', async () => {
+        const form = new FormData();
+        form.append('to', user_1.id);
+        form.append('text', `create message 2`);
+        // user_0 sending a message to user_1
+        const res = await app.inject({
+            method: 'POST',
+            url: '/contact/messages',
+            payload: form,
+            headers: {
+                ...form.getHeaders(),
+                authorization: users[0].jwt,
+            },
+        });
+
+        const message_id = JSON.parse(res.payload).message.id;
+
+        await app.inject({
+            method: 'DELETE',
+            url: `/contact/messages/${message_id}?target=bidirectional`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+
+        const res2 = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_1.id}`,
+            headers: {
+                authorization: users[0].jwt,
+            },
+        });
+        const res3 = await app.inject({
+            method: 'GET',
+            url: `/contact/messages/${user_0.id}`,
+            headers: {
+                authorization: users[1].jwt,
+            },
+        });
+
+        const user_0_messages = JSON.parse(res2.payload).messages;
+        const user_1_messages = JSON.parse(res3.payload).messages;
+
+        expect(user_0_messages.length).toEqual(0);
+        expect(user_1_messages.length).toEqual(2);
     });
 });
