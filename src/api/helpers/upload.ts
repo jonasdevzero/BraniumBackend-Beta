adcbe560ad75268dc0cf1e84ae6bec5f028df470
jonasdevzero <jonasdevzero@gmail.com>
2022-01-17 14:42:08 -0300

    improve upload helper

--- src/api/helpers/upload.ts
+++ src/api/helpers/upload.ts
@@ -1,69 +1,126 @@
-import aws from "aws-sdk"
-import fs from "fs"
-import path from "path"
-import { promisify } from "util"
-import { pipeline, Readable } from "stream"
-import { randomBytes } from "crypto"
+import aws from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import { pipeline, Readable } from 'stream';
+import { randomBytes } from 'crypto';
 
-const s3 = new aws.S3()
-const pump = promisify(pipeline)
-const randomBytesPromisify = promisify(randomBytes)
+const s3 = new aws.S3();
+const pump = promisify(pipeline);
+const randomBytesPromisify = promisify(randomBytes);
 
 export default {
-    save(file: any): Promise<{ Location: string | undefined, type: string }> {
+    save(file: any): Promise<{ Location: string | undefined; type: string }> {
         return new Promise(async (resolve, reject) => {
             try {
-                const { mimetype, filename } = file
-                const type = this._getFileType(mimetype)
+                const { mimetype, filename } = file;
+                const type = this._getFileType(mimetype);
 
-                let hashFilename = await randomBytesPromisify(16).then(hash => `${hash.toString("hex")}-${filename}`)
-                const fileBuffer: Buffer = await file.toBuffer()
+                let hashFilename = await randomBytesPromisify(16).then(
+                    hash => `${hash.toString('hex')}-${filename}`,
+                );
+                const fileBuffer: Buffer = await file.toBuffer();
 
-                if (process.env.STORAGE_TYPE === "s3") {
-                    const result: any = await s3.upload({ Key: hashFilename, Bucket: process.env.AWS_BUCKET || '', Body: fileBuffer }).promise()
-                    resolve({ Location: result.Location, type })
+                if (process.env.STORAGE_TYPE === 's3') {
+                    const result: any = await s3
+                        .upload({
+                            Key: hashFilename,
+                            Bucket: process.env.AWS_BUCKET || '',
+                            Body: fileBuffer,
+                        })
+                        .promise();
+                    resolve({ Location: result.Location, type });
                 } else {
-                    const readable = new Readable()
-                    readable.push(fileBuffer)
-                    readable.push(null)
+                    const readable = new Readable();
+                    readable.push(fileBuffer);
+                    readable.push(null);
 
-                    await pump(readable, fs.createWriteStream(`./uploads/${hashFilename}`))
-                    resolve({ Location: `${process.env.UPLOADS_URL}/${hashFilename}`, type })
+                    await pump(
+                        readable,
+                        fs.createWriteStream(`./uploads/${hashFilename}`),
+                    );
+                    resolve({
+                        Location: `${process.env.UPLOADS_URL}/${hashFilename}`,
+                        type,
+                    });
                 }
             } catch (error) {
-                reject(error)
+                reject(error);
             }
-        })
+        });
     },
 
     remove(key: string) {
         return new Promise(async (resolve, reject) => {
             try {
-                if (process.env.STORAGE_TYPE === "s3") {
-                    const picture_key = key.split(`${process.env.S3_BASE_URL}/`)[1]
-                    s3.deleteObject({ Bucket: "zero-chat", Key: picture_key })        
-                    resolve('ok')
+                if (process.env.STORAGE_TYPE === 's3') {
+                    const picture_key = key.split(
+                        `${process.env.S3_BASE_URL}/`,
+                    )[1];
+                    s3.deleteObject({ Bucket: 'zero-chat', Key: picture_key });
+                    resolve('ok');
                 } else {
-                    const picture_key = key.split(`${process.env.UPLOADS_URL}/`)[1]
-                    await promisify(fs.unlink)(path.resolve(__dirname, "..", "..", "..", "uploads", picture_key))
-                    resolve('ok')
-                };
+                    const picture_key = key.split(
+                        `${process.env.UPLOADS_URL}/`,
+                    )[1];
+                    await promisify(fs.unlink)(
+                        path.resolve(
+                            __dirname,
+                            '..',
+                            '..',
+                            '..',
+                            'uploads',
+                            picture_key,
+                        ),
+                    );
+                    resolve('ok');
+                }
             } catch (error) {
-                reject(error)
+                reject(error);
             }
-        })
+        });
     },
 
-    parseBody(body: any) {
+    parseBody(body: { [key: string]: any }): { [key: string]: any } {
         if (!body) return {};
-        for (const key of Object.keys(body)) 
-            !Array.isArray(body[key]) && !body[key]?.filename ? body[key] = body[key].value : null;
-            
-        return body
+        if (!isCyclic(body)) return body;
+
+        for (const key of Object.keys(body)) {
+            !Array.isArray(body[key]) && !body[key]?.filename
+                ? (body[key] = body[key].value)
+                : null;
+        }
+
+        return body;
     },
 
     _getFileType(mimeType: string) {
-        const type = mimeType.split('/')
-        return type[0] === 'application' ? 'document' : type[0]
+        const type = mimeType.split('/');
+        return type[0] === 'application' ? 'document' : type[0];
+    },
+};
+
+function isCyclic(obj: Object) {
+    let seenObjects: Array<Object> = [];
+
+    function detect(obj: { [key: string]: any }) {
+        if (obj && typeof obj === 'object') {
+            if (seenObjects.indexOf(obj) !== -1) {
+                return true;
+            }
+
+            seenObjects.push(obj);
+            for (var key in obj) {
+                if (
+                    Object.prototype.hasOwnProperty.call(obj, key) &&
+                    detect(obj[key])
+                ) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+
+    return detect(obj);
 }
